4769565d64dfdb7aba624128181f82ea7c9e8570
ShaXBoZ <93701865+shaaxbooz@users.noreply.github.com>
2022-07-12 21:56:11 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -14,7 +14,7 @@ const config = {
 const firebaseApp = firebase.initializeApp(config)
 
 const db = firebaseApp.firestore()
-const testsCollection = db.collection('tests')
+const testsCollection = db.collection('tests3')
 
 export const createTest = test => {
   return testsCollection.add(test)
